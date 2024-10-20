35007ee0455a47b9f9acf4e1ac321827883ff38b
Trần Quốc Đạt <118463964+TripleA00@users.noreply.github.com>
2024-10-20 22:31:09 +0700

    Add new project ex_cpp

---
+++ cpp_ex/ex-1.cpp
@@ -0,0 +1,11 @@
+//viết chương trình đếm xem từ 1-> n có bao nhiêu số chia hết cho 3
+//code the program count for 1 to n , how many number divisible 3
+#include <iostream>
+using namespace std;
+int main(){
+    long long n;
+    cout << "Nhap n : " << endl;
+    cin >> n;
+    cout << "So chia het cho 3 la :" << n/3;
+    return 0;
+}
